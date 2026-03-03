a0295f80c25a34cb07d7c4350f02697002b6e940
jamiird261-collab <jamiird261@gmail.com>
2026-03-02 19:46:43 -0500

    banking done

--- src/main/java/org/codedifferently/savings.java
+++ src/main/java/org/codedifferently/savings.java
@@ -1,6 +1,8 @@
+package org.codedifferently;
+
 public class SavingsAccount extends Account {
 
-    private double interestRate;
+    private final double interestRate;
 
     public SavingsAccount(String accountNumber, double balance, double interestRate) {
         super(accountNumber, balance);
